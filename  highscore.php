c92c66b99099a5a18c946a78b42c45d508eaeab0
NoriKeo <nora.keona@outlook.de>
2023-10-24 14:54:30 +0200

    hi2

---
+++  highscore.php
@@ -0,0 +1,11 @@
+<?php
+
+$name_exists = isset($_POST['name']);
+$score_exists = isset($_POST['score']);
+
+if ($score_exists == true && $name_exists == true)
+    echo 'Die Score für ' . $_POST['name'] . ' ist ' . $_POST['score'];
+else
+    echo 'Keine Score oder Name angegeben!';
+
+?>
